cea7f57b08e03d668180b2aebc6977a2035a59e6
Shyam Gupta <shyamsundar.g@kiprosh.com>
2019-08-15 14:06:38 +0530

    Add Major

--- components/Right.js
+++ components/Right.js
@@ -33,7 +33,7 @@ export default (props) => {
           <div className="content">
             <div className="university"><a href="http://mu.ac.in" target="_blank">Mumbai University</a></div>
             <div className="college"><a href="http://brharnetc.edu.in/br/" target="_blank">B. R. Harne College of Engineering & Technology</a></div>
-            <div className="major"><strong>Major:</strong> Computer Science</div>
+            <div className="major"><strong>Major:</strong> B.E. (Computer Science)</div>
             <div className="grad-date">Graduated in: May 2015</div>
           </div>
         </div>
